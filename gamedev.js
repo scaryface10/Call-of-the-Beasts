8fcb52062ded5aabbd8dcc81a5374c99085c0520
scaryface10 <scaryface10@gmail.com>
2016-06-02 09:22:05 +0800

    trying to make the animals move :D

--- gamedev.js
+++ gamedev.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
 
 //Your jquery goes here
+var score = 0;
 $("#bob").click(function(){
   $("#bob").fadeOut;
   $( "#bob" ).delay( 400 ).fadeOut( 200 );
@@ -12,4 +13,30 @@ $("#bill").click(function(){
   $( "#bill" ).delay( 400 ).fadeOut( 200 );
 
 	});
+
+
+
+    animateDiv();
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() ;
+    var w = $(window).width() ;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv(){
+    var newq = makeNewPosition();
+  //James change .a to .othercar
+    $('.bill').animate({ top: newq[0], left: newq[1] }, function(){
+      animateDiv();        
+    });
+    
+};
 });
