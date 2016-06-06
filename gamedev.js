b7e055deb31e51c5a5c8ebe12455455e894bc5a6
scaryface10 <scaryface10@gmail.com>
2016-06-06 08:27:05 +0800

    taking away the animal that doesn't fit

--- gamedev.js
+++ gamedev.js
@@ -14,31 +14,6 @@ $("#bill").click(function(){
 
 	});
 
-
-  animateDiv();
-
-function makeNewPosition(){
-    
-    // Get viewport dimensions (remove the dimension of the div)
-    var h = $(window).height() ;
-    var w = $(window).width() ;
-    
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
-    
-    return [nh,nw];    
-    
-}
-
-function animateDiv(){
-    var newq = makeNewPosition();
-  //James change .a to .othercar
-    $("#bill").animate({ top: newq[0], left: newq[1] }, function(){
-      animateDiv();        
-    });
-    
-};
-
     animateDiv();
 
 function makeNewPosition(){
@@ -62,4 +37,7 @@ function animateDiv(){
     });
     
 };
+
+
+
 });
